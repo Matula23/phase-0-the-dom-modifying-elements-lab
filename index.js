22a902c64294832c8b770732aa4a135bc5a8a287
Maciej Matula <maciek.matula@gmail.com>
2022-06-06 15:52:38 -0500

    commit tree 805c54f9ab46497babbf28080c0da03f4b1251ba

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Write your code her
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Maciek is the champion"
+
+document.body.append(newHeader)
